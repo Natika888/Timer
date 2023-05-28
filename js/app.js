d7bd315b209b5918358a4a813b37f0f4798615e4
Natika888 <krinata888@gmail.com>
2023-05-28 16:27:47 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+let counterValue = 0;
+let counterInterval;
+
+let buttons = document.querySelectorAll(".button");
+let counter = document.querySelector(".counter");
+// console.log(buttons);
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', onBtnClick);
+})
+
+function onBtnClick(e) {
+    if (e.target.classList.contains('start')) {
+        onStartClick();
+    }
+    if (e.target.classList.contains('stop')) {
+        stopCounter();
+    }
+    if (e.target.classList.contains('reset')) {
+        stopCounter();
+        counterValue = 0;
+        counter.textContent = counterValue;
+    }
+}
+
+function onStartClick(e) {
+    startCounter();
+    
+}
+
+function startCounter() {
+    counterInterval = setInterval(incrementCounter, 1000);
+  }
+
+  function incrementCounter() {
+    counterValue++;
+    counter.textContent = counterValue;
+  }
+
+  function stopCounter() {
+    clearInterval(counterInterval);
+  }
