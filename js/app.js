d42952446c2732ca15eee9072f93c4eaacbd2e5d
Nataliia Krikun <58227698+Natika888@users.noreply.github.com>
2024-10-18 15:00:31 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -46,3 +46,27 @@ function startCounter() {
     clearInterval(counterInterval);
     clickCounter = 0;
   }
+
+  // The wake lock sentinel.
+let wakeLock = null;
+
+// Function that attempts to request a screen wake lock.
+const requestWakeLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request();
+    wakeLock.addEventListener('release', () => {
+      console.log('Screen Wake Lock released:', wakeLock.released);
+    });
+    console.log('Screen Wake Lock released:', wakeLock.released);
+  } catch (err) {
+    console.error(`${err.name}, ${err.message}`);
+  }
+};
+
+// Request a screen wake lock…
+await requestWakeLock();
+// …and release it again after 5s.
+window.setTimeout(() => {
+  wakeLock.release();
+  wakeLock = null;
+}, 5000);
